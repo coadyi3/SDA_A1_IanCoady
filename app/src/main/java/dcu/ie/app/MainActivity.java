86b198a544e41f53ef0534c13f7449e5eba7db7d
coadyi3 <ian.coady3@mail.dcu.ie>
2019-10-09 10:46:46 +0100

    Comment's added to main activity java file

--- app/src/main/java/dcu/ie/app/MainActivity.java
+++ app/src/main/java/dcu/ie/app/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Initial onCreate() method activated once the app starts.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+    //Function added to show a toast msg once button is pressed.
     public void showToast(View view)
     {
         CharSequence text = getResources().getString(R.string.mess_1);
